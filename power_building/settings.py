a1d4f81226929ab28b0f9d5edbffca9bcb5d01c5
Chetram Bassit <chetrambassit121@gmail.com>
2023-02-20 12:36:37 -0500

    secrect key hidden

--- power_building/settings.py
+++ power_building/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rl=5uwvr8c@k3nr$7mt4mmci#^5p95!%-e3fx&d@j=kr=2!7y3"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
